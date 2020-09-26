f99b944f9a31fd54c0aa503bd0c104f4d00c1238
Ignacio <Ignacio.f.l@hotmail.com>
2020-09-26 19:54:46 -0300

    Progress on enemy remaking

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -11,6 +11,7 @@ public class Enemy : MonoBehaviour
     public float attackSpeedRate = 2f;
     public bool isDead = false;
 
+    public GameObject enemyHead;
     public GameObject explosion;
 
     NavMeshAgent agent;
@@ -19,15 +20,19 @@ public class Enemy : MonoBehaviour
     Player playerTarget;
     GameObject platformTarget;
 
+    Rigidbody rb;
+
     float maxSpeedRate;
 
     // Update is called once per frame
     private void Start()    
     {
         agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
 
         maxSpeedRate = attackSpeedRate;
 
+       
         switch (GameManager.Instance.gameMode)
         {
             case GameManager.GameMode.Survival:
@@ -55,27 +60,6 @@ public class Enemy : MonoBehaviour
             agent.speed += Time.deltaTime;
         }
 
-        switch (GameManager.Instance.gameMode)
-        {
-            case GameManager.GameMode.None:
-                break;
-            case GameManager.GameMode.Survival:
-                ChasePlayer();
-                break;
-            case GameManager.GameMode.HoldZone:
-                SearchZone();
-                break;
-            default:
-                break;
-        }
-        
-    }
-
-    void ChasePlayer()
-    {
-        Vector3 distaceToAttack = playerTarget.transform.position - transform.position;
-       
-
         if (health <= 0)
         {
             isDead = true;
@@ -86,10 +70,33 @@ public class Enemy : MonoBehaviour
             Die();
         }
 
+        switch (GameManager.Instance.gameMode)
+        {
+            case GameManager.GameMode.None:
+                break;
+
+            case GameManager.GameMode.Survival:
+                ChasePlayer();
+                break;
+
+            case GameManager.GameMode.HoldZone:
+                SearchZone();
+                break;
+
+            default:
+                break;
+        }
+        
+    }
+
+    void ChasePlayer()
+    {
+        Vector3 distaceToAttack = playerTarget.transform.position - transform.position;
+
         if (playerTarget && !isDead)
         {
-            agent.SetDestination(playerTarget.transform.position - distaceToAttack.normalized);
-            //transform.LookAt(target.transform);
+            agent.SetDestination(playerTarget.transform.position - distaceToAttack.normalized);            
+            enemyHead.transform.LookAt(target.transform);
         }
 
         if (playerTarget.isDead)
