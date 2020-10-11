4be7035b0f12f70f7c4a52c076aec77cf5b26f0b
Ignacio <Ignacio.f.l@hotmail.com>
2020-10-11 19:49:56 -0300

    Added version display

---
+++ Assets/Scripts/UI/GameVersion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameVersion : MonoBehaviour
+{
+    public TextMeshProUGUI versionText;
+    void Start()
+    {
+        versionText.text = "v" + Application.version;
+    }
+}
