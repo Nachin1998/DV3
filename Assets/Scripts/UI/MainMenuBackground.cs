231f38be237024e2fe7768968132debd8fccd5e2
Nachin1998 <ignacio.f.l@hotmail.com>
2020-11-15 01:19:09 -0300

    Made main menu flashscreen. Enhanced enviroment. Completed #417

---
+++ Assets/Scripts/UI/MainMenuBackground.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuBackground : MonoBehaviour
+{
+    Image background;
+    public Sprite normalBackground;
+    public Sprite cursedBackground;
+
+    public float minWaitTime;
+    public float maxWaitTime;
+    public float flashDuration;
+
+    float timer = 0;
+    void Start()
+    {
+        background = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= Random.Range(minWaitTime, maxWaitTime))
+        {
+            StartCoroutine(FlashBackground(flashDuration));
+            timer = 0;
+        }
+    }
+
+    IEnumerator FlashBackground(float flashDuration)
+    {
+        background.sprite = cursedBackground;
+
+        yield return new WaitForSeconds(flashDuration);
+
+        background.sprite = normalBackground;
+    }
+}
