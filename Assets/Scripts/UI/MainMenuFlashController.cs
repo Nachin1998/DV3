888b4a7cd52178d5bab69f51f20d142db9fc784e
Nachin1998 <ignacio.f.l@hotmail.com>
2020-11-24 19:37:49 -0300

    Made main menu extra spooky

---
+++ Assets/Scripts/UI/MainMenuFlashController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuFlashController : MonoBehaviour
+{
+    [Header("Main menu")]
+    public Image menuBackground;
+    public Sprite normalMenuBackground;
+    public Sprite cursedMenuBackground;
+
+    [Header("Credits")]
+    public Image creditsBackground;
+    public Image creditsText;
+    public Sprite normalCreditsBackground;
+    public Sprite cursedCreditsBackground;
+    public Sprite normalCreditsText;
+    public Sprite cursedCreditsText;
+
+    [Space]
+
+    public float minWaitTime;
+    public float maxWaitTime;
+    public float flashDuration;
+
+    float timer = 0;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= Random.Range(minWaitTime, maxWaitTime))
+        {
+            StartCoroutine(FlashScreen(flashDuration));
+            timer = 0;
+        }
+    }
+
+    IEnumerator FlashScreen(float flashDuration)
+    {
+        menuBackground.sprite = cursedMenuBackground;
+        creditsBackground.sprite = cursedCreditsBackground;
+        creditsText.sprite = cursedCreditsText;
+
+        yield return new WaitForSeconds(flashDuration);
+
+        menuBackground.sprite = normalMenuBackground;
+        creditsBackground.sprite = normalCreditsBackground;
+        creditsText.sprite = normalCreditsText;
+    }
+}
